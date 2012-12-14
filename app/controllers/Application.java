24d6b8ce11bb634d388954199a4de776bd92a28a
Sooraj E <sooraj.elamana@attinadsoftware.com>
2012-12-14 13:56:00 +0530

    commit 2

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,21 +3,21 @@ package controllers;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.codehaus.jackson.JsonNode;
+
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
-
-import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
   
-  public static Result index() throws BoilerpipeProcessingException, MalformedURLException {
-	  URL url = new URL("http://www.playframework.org/documentation/2.0.4/Anatomy");
-	   // NOTE: Use ArticleExtractor unless DefaultExtractor gives better results for you
-	   String text = ArticleExtractor.INSTANCE.getText(url);
-    return ok(index.render(text));
+  public static Result index(String extractUrl) throws BoilerpipeProcessingException, MalformedURLException {
+	  URL url = new URL(extractUrl);
+	   JsonNode text = Json.toJson(ArticleExtractor.INSTANCE.getText(url));
+    return ok(index.render(text.asText()));
   }
   
 }

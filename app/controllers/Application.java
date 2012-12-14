780bb0e5bf0689541cb51a08e3127c469dd5f86c
Sooraj E <sooraj.elamana@attinadsoftware.com>
2012-12-14 12:53:20 +0530

    boilerpipe location change

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,11 @@
 package controllers;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
+
 import play.*;
 import play.mvc.*;
 
@@ -7,8 +13,11 @@ import views.html.*;
 
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
+  public static Result index() throws BoilerpipeProcessingException, MalformedURLException {
+	  URL url = new URL("http://www.playframework.org/documentation/2.0.4/Anatomy");
+	   // NOTE: Use ArticleExtractor unless DefaultExtractor gives better results for you
+	   String text = ArticleExtractor.INSTANCE.getText(url);
+    return ok(index.render(text));
   }
   
 }
